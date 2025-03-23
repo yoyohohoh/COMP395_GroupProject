c2044f20162ea0e83088a881f52a94eedeac261b
school <yho45@my.centennialcollege.ca>
2025-03-22 20:49:27 -0400

    scene transition

--- TimHortons/Assets/_Scripts/GuideController.cs
+++ TimHortons/Assets/_Scripts/GuideController.cs
@@ -3,7 +3,19 @@ using UnityEngine;
 public class GuideController : MonoBehaviour
 {
     public GameObject guideArrowPrefeb;
-
+    public GameObject gameButton;
+    DataKeeper dataKeeper;
+    private void Start()
+    {
+        dataKeeper = DataKeeper.Instance;
+    }
+    private void Update()
+    {
+        if(dataKeeper.taskList.Count >= dataKeeper.totalTask)
+        {
+            gameButton.SetActive(true);
+        }
+    }
     public void InstantiateGuide(Vector3 spawnPosition)
     {
         Instantiate(guideArrowPrefeb, spawnPosition, Quaternion.identity);
